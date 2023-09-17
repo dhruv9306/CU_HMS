d680720e2f822477005fdb26432b2dbdd566ae40
dhruv9306 <142716205+dhruv9306@users.noreply.github.com>
2023-09-17 22:28:32 +0530

    Update form_handle.php

--- form_handle.php
+++ form_handle.php
@@ -4,19 +4,20 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@cumail.in';
+$email_from = 'dhruvgandhi982003@gmail.com';
 
 $email_subject = 'New Form Submission';
 
 $email_body = "User Name:$name.\n " .
     "User Email:$visitor_email .\n " .
     "Subject:$subject.\n" .
-    "User Message:$visitor_email .\n ";
+    "User Message:$message .\n ";
 
 
 $to = 'dhruvgandhi870842@gmail.com';
-$headers = " From: $email_form \r\n";
+$headers = " From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to, $email_subject, $email_body, $headers);
 header("Location: contact.html");
+?>
